71d23ff49a21f41a2870dd62d41fc9016c156031
Parashuram <n.parashuram@yahoo.co.in>
2010-12-10 17:53:08 +0530

    Added Web Worker Functionality for Seam Processing

--- SeamCarver.js
+++ SeamCarver.js
@@ -1,15 +1,17 @@
-function SeamCarver(imgData, adjust, callback){
-    var newImage = resizeReduce(imgData, adjust, seam.topDownSeam);
+self.addEventListener("message", function(e){
+    SeamCarver(e.data.image, e.data.adjust, function(d){
+        self.postMessage(d);
+    });
+}, false);
+
+function SeamCarver(imgData, dim, callback){
+    importScripts("seamJSON.js");
+    var newImage = resizeReduce(imgData, dim, seam.topDownSeam);
     callback(newImage);
 }
 
 var resizeReduce = function(image, dim, seams){
-    var newImage = {
-        "height": image.height,
-        "width": image.width,
-        "data": []
-    };
-    
+    var data = [];
     var getPixel = function(x, y){
         var base = (y * image.width + x) * 4;
         return {
@@ -21,15 +23,14 @@ var resizeReduce = function(image, dim, seams){
     }
     
     var putPixel = function(x, y, rgba){
-        var base = (y * newImage.width + x) * 4;
-        newImage.data[base + 0] = rgba.red;
-        newImage.data[base + 1] = rgba.blue;
-        newImage.data[base + 2] = rgba.green;
-        newImage.data[base + 3] = rgba.alpha;
+        var base = (y * image.width + x) * 4;
+        data[base + 0] = rgba.red;
+        data[base + 1] = rgba.blue;
+        data[base + 2] = rgba.green;
+        data[base + 3] = rgba.alpha;
     };
     
     var widthDiff = image.width - dim.width;
-    
     for (var y = 0; y < image.height; y++) {
         var x1 = 0; // x counter of the new image
         for (var x = 0; x < image.width; x++) {
@@ -50,14 +51,17 @@ var resizeReduce = function(image, dim, seams){
     
     for (var x = dim.width; x < image.width; x++) {
         for (var y = 0; y < image.height; y++) {
-            var base = (y * newImage.width + x) * 4;
-            newImage.data[base + 0] = 0;
-            newImage.data[base + 1] = 0;
-            newImage.data[base + 2] = 0;
-            newImage.data[base + 3] = 0;
+            var base = (y * image.width + x) * 4;
+            data[base + 0] = 0;
+            data[base + 1] = 0;
+            data[base + 2] = 0;
+            data[base + 3] = 0;
         }
     }
-    return newImage;
+    
+    return {
+        "width": image.width,
+        "height": image.height,
+        "data": data
+    };
 };
-
-
