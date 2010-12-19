7c0d812a491b66cd4660ea7cefe3ef5d4b5ff5c0
Parashuram <n.parashuram@yahoo.co.in>
2010-12-19 18:27:18 +0530

    Refactored code with prototypes

--- SeamCarver.js
+++ SeamCarver.js
@@ -1,82 +1,85 @@
 self.addEventListener("message", function(e){
-    var x = SeamCarver(e.data.image, e.data.adjust);
-    x.data.action = "done";
-    self.postMessage(x);
+    var seamCarver = (new SeamCarver(e.data.image));
+    var result = seamCarver.resize(e.data.adjust);
+    result.action = "done";
+    self.postMessage(result);
 }, false);
 
-function log(msg){
+var log = (log ||
+function(msg){
     self.postMessage(msg);
+});
+
+function SeamCarver(image){
+    this.image = image;
+    this.newImage = [];
+    log("Starting Seam Carving");
+    this.getSeams();
 }
 
-function SeamCarver(imgData, dim, callback){
-    var energy = [];
-    log("Starting Seam Carving");
-    var getEnergy = function(x, y){
-    
+SeamCarver.prototype.getPixel = function(x, y){
+    var base = (y * this.image.width + x) * 4;
+    return {
+        "red": this.image.data[base + 0],
+        "blue": this.image.data[base + 1],
+        "green": this.image.data[base + 2],
+        "alpha": this.image.data[base + 3]
     }
-    
-    function getSeams(imgData){
-        importScripts("seamJSON.js");
-        return seam.topDownSeam;
-    }
-    
-    function resizeReduce(image, dim, seams){
-        log("Starting resize Reduce");
-        var data = [];
-        var getPixel = function(x, y){
-            var base = (y * image.width + x) * 4;
-            return {
-                "red": image.data[base + 0],
-                "blue": image.data[base + 1],
-                "green": image.data[base + 2],
-                "alpha": image.data[base + 3]
+}
+
+SeamCarver.prototype.putPixel = function(x, y, rgba){
+    var base = (y * this.image.width + x) * 4;
+    this.newImage[base + 0] = rgba["red"];
+    this.newImage[base + 1] = rgba["blue"];
+    this.newImage[base + 2] = rgba["green"];
+    this.newImage[base + 3] = rgba["alpha"];
+    return this;
+};
+
+SeamCarver.prototype.getSeams = function(){
+    importScripts("seamJSON.js");
+    this.seams = self.seam.topDownSeam;
+    return this;
+}
+
+SeamCarver.prototype.resize = function(dim){
+    var image = this.image;
+    log("Starting resize Reduce");
+    this.newImage = [];
+    var widthDiff = image.width - dim.width;
+    for (var y = 0; y < image.height; y++) {
+        var x1 = 0; // x counter of the new image
+        for (var x = 0; x < image.width; x++) {
+            this.putPixel(x, y, this.getPixel(x, y));
+            var isSkippable = false;
+            for (var i = 0; i < widthDiff; i++) {
+                if (this.seams[i][y] == x) {
+                    isSkippable = true;
+                    break;
+                }
+            }
+            if (isSkippable == false) {
+                this.putPixel(x1, y, this.getPixel(x, y));
+                x1++;
             }
         }
-        
-        var putPixel = function(x, y, rgba){
-            var base = (y * image.width + x) * 4;
-            data[base + 0] = rgba.red;
-            data[base + 1] = rgba.blue;
-            data[base + 2] = rgba.green;
-            data[base + 3] = rgba.alpha;
-        };
-        
-        var widthDiff = image.width - dim.width;
+    }
+    
+    for (var x = dim.width; x < image.width; x++) {
         for (var y = 0; y < image.height; y++) {
-            var x1 = 0; // x counter of the new image
-            for (var x = 0; x < image.width; x++) {
-                putPixel(x, y, getPixel(x, y));
-                var isSkippable = false;
-                for (var i = 0; i < widthDiff; i++) {
-                    if (seams[i][y] == x) {
-                        isSkippable = true;
-                        break;
-                    }
-                }
-                if (isSkippable == false) {
-                    putPixel(x1, y, getPixel(x, y));
-                    x1++;
-                }
-            }
+            this.putPixel(x, y, {
+                "red": 0,
+                "blue": 0,
+                "alpha": 0,
+                "green": 0
+            });
         }
-        
-        for (var x = dim.width; x < image.width; x++) {
-            for (var y = 0; y < image.height; y++) {
-                var base = (y * image.width + x) * 4;
-                data[base + 0] = 0;
-                data[base + 1] = 0;
-                data[base + 2] = 0;
-                data[base + 3] = 0;
-            }
-        }
-        
-        return {
-            "width": image.width,
-            "height": image.height,
-            "data": data
-        };
     }
     
-    return resizeReduce(imgData, dim, getSeams(imgData));
+    return {
+        "width": image.width,
+        "height": image.height,
+        "data": this.newImage
+    };
 }
 
